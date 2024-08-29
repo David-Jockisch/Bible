3099e6ac43066c6c4cf5c8a7b5ef77f943c38824
David Jockisch <david.jockisch@gmail.com>
2024-08-29 17:37:30 -0500

    css and cache changes

--- sw.js
+++ sw.js
@@ -1,22 +1,32 @@
+// Install event: Comment out caching logic for development
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('proverbs-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
+  // Commented out caching for development
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+  event.waitUntil(
+    caches.open('proverbs-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ]);
+    })
+  );
+
+});
+
+// Fetch event: Comment out caching logic for development
+self.addEventListener('fetch', function(event) {
+  // Commented out caching logic for development
   
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+  
+  
+  // Always fetch from the network
+  event.respondWith(fetch(event.request));
+});
