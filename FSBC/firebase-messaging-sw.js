97698490a755916cd5c04ff2a57cd80d03b9a41c
David Jockisch <david.jockisch@gmail.com>
2025-03-02 16:06:49 -0600

    6

--- FSBC/firebase-messaging-sw.js
+++ FSBC/firebase-messaging-sw.js
@@ -4,15 +4,15 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
 // Initialize Firebase in the service worker
         // Firebase configuration
         const firebaseConfig = {
-            apiKey: "AIzaSyD_5MWAYSC-7nYHzWzcC99rP9xOb85-cvs",
-            authDomain: "fsbc-rushville.firebaseapp.com",
-            databaseURL: "https://fsbc-rushville-default-rtdb.firebaseio.com",
-            projectId: "fsbc-rushville",
-            storageBucket: "fsbc-rushville.firebasestorage.app",
-            messagingSenderId: "363958219468",
-            appId: "1:363958219468:web:63f4f4be8e7e827d4c4869",
-            measurementId: "G-JEHC1TWY12"
-    };
+        apiKey: "AIzaSyD_5MWAYSC-7nYHzWzcC99rP9xOb85-cvs",
+        authDomain: "fsbc-rushville.firebaseapp.com",
+        databaseURL: "https://fsbc-rushville-default-rtdb.firebaseio.com",
+        projectId: "fsbc-rushville",
+        storageBucket: "fsbc-rushville.firebasestorage.app",
+        messagingSenderId: "363958219468",
+        appId: "1:363958219468:web:63f4f4be8e7e827d4c4869",
+        measurementId: "G-JEHC1TWY12"
+};
 
 firebase.initializeApp(firebaseConfig);
 
